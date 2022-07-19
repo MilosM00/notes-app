f4292879907f0acf319342c8b481aca9b66da5fe
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-19 09:46:21 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { nanoid } from "nanoid";
+import NoteList from "./components/NoteList";
 
 const App = () =>{
+
+    const [notes, setNotes] = React.useState([
+        {
+            id: nanoid(),
+            text: "This is test one!",
+            date: "19.07.2022.",
+        },
+        
+        {
+            id: nanoid(),
+            text: "This is test two!",
+            date: "18.10.2022.",
+        },
+
+        {
+            id: nanoid(),
+            text: "This is test three!",
+            date: "28.07.2022.",
+        }
+
+    ]);
+
     return(
 
-        <div>
+        <div className="app">
+
+            <NoteList 
+                notes={notes}
+            />
             
         </div>
 
