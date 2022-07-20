290e2a3a9b976b935dcb279042f5aa7304225122
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-20 09:54:15 +0200

    note-list-js updated

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,8 +1,29 @@
 import React from "react";
 import Note from "./Note";
+import AddNote from "./AddNote";
 import "./NoteList.css";
 
-const NoteList = ({ notes }) =>{
+const NoteList = ({ notes, addNoteEvent, deleteNoteEvent }) =>{
+
+    const maxLength = 200;
+
+    const [noteText, setNoteText] = React.useState("");
+
+    const changeEvent = (e) =>{
+        if(maxLength - e.target.value.length >= 0)
+        {
+            setNoteText(e.target.value);
+        }
+    };
+
+    const saveEvent = () =>{
+        if(noteText.trim().length > 0)
+        {
+            addNoteEvent(noteText);
+            setNoteText("");
+        }
+    };
+
     return(
 
         <div className="note-list">
@@ -10,12 +31,21 @@ const NoteList = ({ notes }) =>{
             {notes.map((note) => (
 
                 <Note 
+                    key={note.id}
                     id={note.id}
                     text={note.text}
                     date={note.date}
+                    deleteNoteEvent={deleteNoteEvent}
                 />
-            
+    
             ))}
+
+            <AddNote 
+                changeEvent={changeEvent}
+                noteText={noteText}
+                saveEvent={saveEvent}
+                maxLength={maxLength}
+            />
 
         </div>
 
